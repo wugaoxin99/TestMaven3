9548f7d617c4276139005bcde28c74fd5ee5e2f5
吴高新 <wugaoxin999@163.com>
2020-08-03 22:16:55 +0800

    Initial commit

---
+++ src/main/java/com/wugaoxin/util/Cat.java
@@ -0,0 +1,11 @@
+package com.wugaoxin.util;
+
+public class Cat {
+    public static void eat(){
+        System.out.println("cat eat eat eat...");
+        System.out.println("11111111111");
+    }
+    public static void go(){
+        System.out.println("It's time to sleep");
+    }
+}
